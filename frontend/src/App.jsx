09c7d4a8b38af9578fbc5b2170df0f210d7bbf6a
GrimProg <vavilovila01@gmail.com>
2025-09-14 00:16:12 +0600

    Add router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
-import './App.css'
+import "./App.css"
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
-
+  return(
+    <Routes>
+      <Route></Route>
+    </Routes>
+  )
 
 }
 
