cf39b5394e8a51acc550881fb9fdf5c9e9a200ee
GrimProg <vavilovila01@gmail.com>
2025-09-14 00:35:27 +0600

    Добавил шапку и минимальную навигацию

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,23 @@
-import "./App.css"
-import { Route, Routes } from 'react-router-dom'
+import "./App.css";
+import React, { lazy, Suspense } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+
+import Header from "./components/header/Header";
+const Home = lazy(() => import("./components/home/Home"));
 
 function App() {
-  return(
-    <Routes>
-      <Route></Route>
-    </Routes>
-  )
+  return (
+    <>
+      <Header />
+      <Suspense fallback={<div>Загрузка...</div>}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />} />
 
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Suspense>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom"
+
+export default function Header() {
+    return(
+        <div>
+            <Link to="/home">Home</Link>
+        </div>
+    )
+}
